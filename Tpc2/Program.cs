8650c9238e522b66ef01ec81d9a3e37d0691921d
HenriqueFonseca-Isel <72544181+HenriqueFonseca-Isel@users.noreply.github.com>
2021-03-18 20:21:30 +0100

    Add files via upload

---
+++ Tpc2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+
+namespace Tpc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try{
+                 Assembly asm =  Assembly.LoadFrom(@"C:\Users\user\Desktop\Isel\AVE\Tpc2\RestSharp.dll");
+
+                
+                foreach(Type a in asm.GetTypes() ){
+                      Console.WriteLine(a);
+                     foreach( MethodInfo c in a.GetMethods())
+                        Console.WriteLine(c);}
+            
+            }catch(Exception b){
+                 Console.WriteLine("Holla");
+            }
+                
+
+
+            Console.WriteLine("Hello World!");
+
+        
+
+        }
+    }
+}
